5d319faaf0ec90da3db5e8d6fc28fce71004a7be
singhveerraj <72805099+singhveerraj@users.noreply.github.com>
2020-11-21 09:40:18 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,33 @@
+class Paper {
+constructor(x,y,radius){
+    var options = {
+        restitution:0.5,
+    
+        friction:0.5,
+        density:1.2
+    }
+    this.Body = Bodies.circle(x,y,radius,options);
+    this.radius = radius;
+    this.image = loadImage("sprites/paper.png")
+
+   World.add(world,this.Body);
+
+
+
+
+}
+display(){
+    var pos = this.Body.position;
+    fill ("white");
+    ellipseMode(CENTER);
+    ellipse(pos.x,pos.y,this.radius);
+}
+
+
+
+
+
+
+
+}
+
